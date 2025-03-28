6bb96f552c49c8fb7b8dfca23dc619e3e8a26aac
donlim <donlim@berkeley.edu>
2025-03-27 19:16:13 -0700

    updated firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDzYYCwGtjznPNvMfGSmQam8sTF3JLbEa0",
+  authDomain: "joy-chat-app-9a9e0.firebaseapp.com",
+  projectId: "joy-chat-app-9a9e0",
+  storageBucket: "joy-chat-app-9a9e0.appspot.com", // ✅ fixed here
+  messagingSenderId: "1037213212817",
+  appId: "1:1037213212817:web:114c138e1cf505c091df30",
+  measurementId: "G-0ZRNTTBVQK"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
